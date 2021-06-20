d7f6040db254e9dcfb2a1d29ea4d420dae8ceae3
JoelMichaelRutter <joel-rutter@live.co.uk>
2021-06-20 11:09:34 +0000

    add first basic nested data structure to questions.js, can use this template now to populate the rest of the data structure.

---
+++ assets/javascript/questions.js
@@ -0,0 +1,13 @@
+let questions = [
+    {
+    numb: 1,
+    situation: "BREAKING NEWS! There is an outbreak of a new deadly virus that kills the infected and reanimates the body. All major cities are in chaos and teeming with the undead. Law and order has gone out the window – looting and destruction is rife.",
+    question: "How would you react!?",
+    options: {
+        40: 'Listen to the news carefully and make a plan of action',
+        30: 'Join in the looting and embrace the chaos',
+        20: 'Panic and lock yourself in a cupboard – no one will find me here',
+        10: 'Get drunk as a skunk and hope for the best',
+    }
+  },
+]
