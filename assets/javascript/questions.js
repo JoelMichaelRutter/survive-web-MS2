f2078aa910feb423b40cf95e746e49e2a2b15b9a
JoelMichaelRutter <joel-rutter@live.co.uk>
2021-06-20 11:23:15 +0000

    add additonal objects into questions.js to serve as the questions in the game, these will be pulled into their respective containers in the HTML document

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -10,4 +10,59 @@ let questions = [
         10: 'Get drunk as a skunk and hope for the best',
     }
   },
+  {
+    numb: 2,
+    situation: "You know deep down that you need to leave, so you grab a bag and get ready to face your fears.",
+    question: "What do you pack?",
+    options: {
+        40: 'The essentials - water, food, first aid kit and a torch',
+        30: 'Your pet',
+        20: 'Nothing',
+        10: 'Your favourite clothes',
+    }
+  },
+  {
+    numb: 3,
+    situation: "You narrowly escape. You came closer to the hoards of the undead than you were comfortable with but you’re safe - for now...",
+    question: "Where are you heading?",
+    options: {
+        40: 'The closest military base',
+        30: 'The Isle of Wight',
+        20: 'The countryside',
+        10: 'The coast',
+    }
+  },
+  {
+    numb: 4,
+    situation: "You learn that the only way make the undead dead again is to destroy the brain.",
+    question: "What would be your weapon of choice?",
+    options: {
+        40: 'Cricket bat',
+        30: 'Hunting rifle',
+        20: 'Umbrella',
+        10: 'Garden strimmer',
+    }
+  },
+  {
+    numb: 5,
+    situation: "You have run out of food.",
+    question: "What is your preferred plan of action?",
+    options: {
+        40: 'Scavenge for food',
+        30: 'Lay traps',
+        20: 'Cut of your arm and eat it',
+        10: 'Eat a zombie',
+    }
+  },
+  {
+    numb: 6,
+    situation: "One of your group has a bite on their arm, no one else knows other than you.",
+    question: "What do you do?",
+    options: {
+        40: 'End them...',
+        30: 'Banish them from your group',
+        20: 'Call a group meeting',
+        10: 'Ignore it and hope it was a dog',
+    }
+  },
 ]
