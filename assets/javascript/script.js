35a1f198c5fc74df4084f7de9fc90d3d7551c3e3
JoelMichaelRutter <joel-rutter@live.co.uk>
2021-06-30 14:25:24 +0000

    add audio element with audio file to index.html, prepared some script functionality in script.js to handle playing and pausing music but hitting an error

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,6 +10,19 @@ let gameArea = document.getElementById('game-container');
 let answerContainer = document.getElementById('answer-btn-container');
 playButton.addEventListener('click', startGame);
 
+let backgroundMusic = document.getElementsByTagName('audio');
+let playMusicButton = document.getElementById('play-music');
+playMusicButton.addEventListener('click', playMusic());
+let muteButton = document.getElementById('mute-music');
+muteButton.addEventListener('click', pauseMusic())
+
+function playMusic(){
+    backgroundMusic.play();
+}
+
+function pauseMusic(){
+    backgroundMusic.pause();
+}
 /**
  * This function runs the intial sequence to start the game on click of the start-game button, from here the change question 
  * function will take over to populate the HTML from the questions.js file each time an 
@@ -23,6 +36,7 @@ function startGame() {
     let answerContainer = document.getElementById('answer-btn-container')
     answerContainer.style.display = 'flex';
     changeQuestionAndOptions();
+    playMusic();
 }
 /**
  * The function and event listeners  change the question when the user 
@@ -107,11 +121,17 @@ function assessScore() {
     }
 }
 
+/**
+ * The code below grabs the play again buttons from the results divs and and adds an event listener to each which calls the restart game function.
+ */
 let playAgainBtns = Array.from(document.getElementsByClassName('play-again-btn'));
 playAgainBtns.forEach(button => button.addEventListener('click', () => {
     restartGame();
 }));
 
+/**
+ * This function reloads the document on click of a play again button which will then let the user run through the quiz again.
+ */
 function restartGame(){
     document.location.href = "";
 }
